7a6499909d5ed2d359fc7859e4887ccbc09dd541
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2026-02-15 14:29:54 +0100

    Delete the adhoc file based storage engine

--- src/getInventoryFiles.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "node:fs/promises";
-import * as path from "node:path";
-import { DATA_DIR } from "./DATA_DIR.ts";
-
-export async function getInventoryFiles() {
-  return (await fs.readdir(DATA_DIR))
-    .filter((f) => path.extname(f) === ".json")
-    .map((f) => [f, path.join(DATA_DIR, f)] as const);
-}
